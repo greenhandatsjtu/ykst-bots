f35fbd7489028b1f01260536add2e8f7b8fd2af8
greenhandatsjtu <sunhengke@sjtu.edu.cn>
2022-04-23 18:39:02 +0800

    connect to treehole server

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,17 @@
-fn main() {
-    println!("Hello, world!");
+use model::tree_hole_client::TreeHoleClient;
+use model::{EmptyRequest};
+
+pub mod model {
+    tonic::include_proto!("model");
+}
+
+const API_URL: &str = "https://api.treehole.dyweb.sjtu.cn";
+
+#[tokio::main]
+async fn main() -> Result<(), Box<dyn std::error::Error>> {
+    let mut client = TreeHoleClient::connect(API_URL).await?;
+    let request = tonic::Request::new(EmptyRequest {});
+    let response = client.ping(request).await?;
+    println!("RESPONSE={:?}", response);
+    Ok(())
 }
