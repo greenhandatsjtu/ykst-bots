5b65111fa4e83b2f5ba8fafe4536659035f926f4
greenhandatsjtu <sunhengke@sjtu.edu.cn>
2022-04-24 19:07:23 +0800

    add bin to get ykst token; use token to fetch profile

---
+++ src/bot.rs
@@ -0,0 +1,47 @@
+use model::tree_hole_client::TreeHoleClient;
+use model::*;
+use tonic::{Status, transport::{Endpoint}};
+use config::Config;
+use tonic::service::{Interceptor};
+
+pub mod model {
+    tonic::include_proto!("model");
+}
+
+struct AuthInterceptor {
+    token: String
+}
+
+impl Interceptor for AuthInterceptor {
+    fn call(&mut self, mut request: tonic::Request<()>) -> Result<tonic::Request<()>, Status> {
+        // insert treehole token
+        request.metadata_mut().insert("authorization", self.token.parse().unwrap());
+        Ok(request)
+    }
+}
+
+#[tokio::main]
+async fn main() -> Result<(), Box<dyn std::error::Error>> {
+    let settings = Config::builder()
+        .add_source(config::File::with_name("config.yaml"))
+        .build()?;
+    let api_url = settings.get_string("API_URL")?;
+    let token = settings.get_string("TREEHOLE_TOKEN")?;
+    println!("{}", api_url);
+    // let token = settings.get("YKST_TOKEN")?;
+    let channel = Endpoint::from_shared(api_url)?.connect().await?;
+
+    let mut client = TreeHoleClient::with_interceptor(channel, AuthInterceptor{token});
+
+    println!("Ping server...");
+    let request = tonic::Request::new(EmptyRequest {});
+    let response = client.ping(request).await?;
+    println!("RESPONSE={:?}", response);
+
+    println!("Get profile...");
+    let request = tonic::Request::new(EmptyRequest {});
+    let response = client.get_profile(request).await?;
+    println!("RESPONSE={:?}", response);
+
+    Ok(())
+}
---
+++ src/get-token.rs
@@ -0,0 +1,43 @@
+use model::tree_hole_client::TreeHoleClient;
+use model::*;
+use std::env;
+use reqwest;
+
+pub mod model {
+    tonic::include_proto!("model");
+}
+
+const API_URL: &str = "https://proxy.treehole.dyweb.sjtu.cn";
+
+#[tokio::main]
+async fn main() -> Result<(), Box<dyn std::error::Error>> {
+    let mut client = TreeHoleClient::connect(API_URL).await?;
+
+    println!("Fetching oauth config...");
+    let request = tonic::Request::new(OAuthConfigRequest {
+        channel: OAuthLoginChannel::LoginWithJAccount as i32,
+        source: LoginSource::Web as i32,
+    });
+    let config = client.get_o_auth_config(request).await?.into_inner();
+
+    println!("Getting code...");
+    let http_client = reqwest::Client::new();
+    let url = format!("{}?response_type=code&client_id={}&scope={}&redirect_uri=https://web.treehole.space/auth/jaccount", config.authorize_url, config.client_id, config.scopes[0]);
+    println!("{}", url);
+    let cookie = env::var("JACCOUNT_COOKIE").expect("JACCOUNT_COOKIE env not presented");
+    let response = http_client.get(url).header(reqwest::header::COOKIE, cookie).send().await?;
+    let url: String = response.url().to_string();
+    let code = url.split("code=").collect::<Vec<&str>>()[1].to_string();
+    println!("Code: {}", code);
+
+    println!("Login to ykst...");
+    let request = tonic::Request::new(OAuthLoginRequest {
+        code,
+        channel: OAuthLoginChannel::LoginWithJAccount as i32,
+        source: LoginSource::Web as i32,
+        web_source: WebSource::ProdServer as i32,
+    });
+    let token = client.o_auth_login(request).await?.into_inner().token;
+    println!("Token: {}", token);
+    Ok(())
+}
