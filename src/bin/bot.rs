71ea4ee9bc5a8de2b50583c89835be99c3593ae4
greenhandatsjtu <sunhengke@sjtu.edu.cn>
2022-04-25 15:22:56 +0800

    reformat project structure

---
+++ src/bin/bot.rs
@@ -0,0 +1,35 @@
+mod ykst_bot;
+
+use config::Config;
+
+pub mod model {
+    tonic::include_proto!("model");
+}
+
+#[tokio::main]
+async fn main() -> Result<(), Box<dyn std::error::Error>> {
+    let settings = Config::builder()
+        .add_source(config::File::with_name("config.yaml"))
+        .build()?;
+    let api_url = settings.get_string("API_URL")?;
+    let token = settings.get_string("TREEHOLE_TOKEN")?;
+    let identity = settings.get_string("IDENTITY_CODE")?;
+
+    let mut bot = ykst_bot::Bot::new(api_url, token, identity).await?;
+
+    let _user = bot.get_profile().await?;
+
+    let threads = bot.get_user_threads().await?;
+    println!("{:?}", threads.threads[0]);
+
+    let title = String::from("test title");
+    let content = String::from("test content");
+    let thread = bot.create_thread(1, title, content).await?;
+    println!("{:#?}", thread);
+
+    let content = String::from("test reply");
+    let post = bot.reply_to_thread(5227, content).await?;
+    println!("{:#?}", post);
+
+    Ok(())
+}
