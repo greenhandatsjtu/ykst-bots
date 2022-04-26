f747f93e97ccbfeaa15fac684e45f8ed7c3eac03
greenhandatsjtu <sunhengke@sjtu.edu.cn>
2022-04-26 18:48:34 +0800

    rename library

--- bots/src/bin/demo-bot.rs
+++ bots/src/bin/demo-bot.rs
@@ -1,4 +1,4 @@
-use ykst_bot;
+use ykst_client;
 use config::Config;
 
 #[tokio::main]
@@ -10,20 +10,20 @@ async fn main() -> Result<(), Box<dyn std::error::Error>> {
     let token = settings.get_string("TREEHOLE_TOKEN")?;
     let identity = settings.get_string("IDENTITY_CODE")?;
 
-    let mut bot = ykst_bot::Bot::new(api_url, token, identity).await?;
+    let mut client = ykst_client::Client::new(api_url, token, identity).await?;
 
-    let _user = bot.get_profile().await?;
+    let _user = client.get_profile().await?;
 
-    let threads = bot.get_user_threads().await?;
+    let threads = client.get_user_threads().await?;
     println!("{:?}", threads.threads[0]);
 
     let title = String::from("test title");
     let content = String::from("test content");
-    let thread = bot.create_thread(ykst_bot::Category::Main, title, content, None).await?;
+    let thread = client.create_thread(ykst_client::Category::Main, title, content, None).await?;
     println!("{:#?}", thread);
 
     let content = String::from("test reply");
-    let post = bot.reply_to_thread(5227, content).await?;
+    let post = client.reply_to_thread(5227, content).await?;
     println!("{:#?}", post);
 
     Ok(())
